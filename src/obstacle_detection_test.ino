3230255baf5956ee6aea014586a6fee91e63af40
Vinícius <viniramos4442008@gmail.com>
2026-05-16 20:52:12 -0300

    Refactor obstacle detection to control motors
    Added a distance-based haptic feedback system and implemented a dual motor control structure

--- src/obstacle_detection_test.ino
+++ src/obstacle_detection_test.ino
@@ -1,21 +1,19 @@
 const int TRIG = 5;
 const int ECHO = 18;
-const int vibrationMotor = 23;
+const int rightMotor = 23;
+const int leftMotor = 22;
 
-void setup()
-{
+// C++ code
+//
+void setup() {
   Serial.begin(115200);
-
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
-
-  pinMode(vibrationMotor, OUTPUT);
-
-  digitalWrite(TRIG, LOW);
+  pinMode(rightMotor, OUTPUT);
+  pinMode(leftMotor, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
 
@@ -29,17 +27,30 @@ void loop()
   float distance = duration * 0.034 / 2;
 
   Serial.print("Distance: ");
-  Serial.print(distance, 1);
+  Serial.print(distance);
   Serial.println(" cm");
 
-  if (distance < 20)
-  {
-    digitalWrite(vibrationMotor, HIGH);
-  }
-  else
-  {
-    digitalWrite(vibrationMotor, LOW);
-  }
 
+  if (distance > 70) {
+    digitalWrite(rightMotor, LOW);
+    digitalWrite(leftMotor, LOW);
+  } else if (distance > 30) {
+    digitalWrite(rightMotor, HIGH);
+    digitalWrite(leftMotor, HIGH);
+    delay(500);
+    digitalWrite(rightMotor, LOW);
+    digitalWrite(leftMotor, LOW);
+    delay(500);
+  } else if (distance > 10) {
+    digitalWrite(rightMotor, HIGH);
+    digitalWrite(leftMotor, HIGH);
+    delay(100);
+    digitalWrite(rightMotor, LOW);
+    digitalWrite(leftMotor, LOW);
+    delay(100);
+  } else {
+    digitalWrite(rightMotor, HIGH);
+    digitalWrite(leftMotor, HIGH);
+  }
   delay(300);
 }
